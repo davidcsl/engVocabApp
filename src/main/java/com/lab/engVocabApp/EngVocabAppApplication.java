b1553bbd0732f126c2dab3429cae5222d99b4637
davidcsl <singaporecsl123@gmail.com>
2022-10-29 19:45:39 +0800

    first commit

---
+++ src/main/java/com/lab/engVocabApp/EngVocabAppApplication.java
@@ -0,0 +1,13 @@
+package com.lab.engVocabApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngVocabAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngVocabAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/engVocabApp/EngVocabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.engVocabApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngVocabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
